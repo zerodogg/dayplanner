03a6653add30b3b9864413094682ecd550ae6cf4
Eskild Hustvedt <code@zerodogg.org>
2007-07-29 19:18:36 +0000

    Added a simple HTTP fetcher. Currently supports lynx/wget/curl

--- modules/DP-GeneralHelpers/lib/DP/GeneralHelpers.pm
+++ modules/DP-GeneralHelpers/lib/DP/GeneralHelpers.pm
@@ -126,6 +126,12 @@ sub AppendZero {
 our $VERSION;
 $VERSION = 0.1;
 
+# Purpose: Find out if a command is in PATH or not
+# Usage: InPath(COMMAND);
+sub InPath {
+	foreach (split /:/, $ENV{PATH}) { if (-x "$_/@_" and ! -d "$_/@_" ) {   return 1; } } return 0;
+}
+
 # ----
 # IPC HANDLER
 # ---
@@ -520,6 +526,136 @@ sub AMPM_From24 {
 	}
 	return("$Hour:$Minutes $Suffix");
 }
+package DP::GeneralHelpers::HTTPFetch;
+use IPC::Open3;
+use POSIX;
+use Socket;
+
+sub get {
+	my $self = shift;
+	my $file = shift;
+	my $progress = shift;
+
+	# First we try to resolve the IP. If this fails we just
+	# error out.
+	my $addy = $file;
+	$addy =~ s#^\w+://##;
+	$addy =~ s#^(\w+)/.*##;
+	my $resolved = inet_ntoa(inet_aton($addy));
+	if(not $resolved) {
+		return('NORESOLVE');
+	}
+
+	# First try LWP
+	#  TODO: LWP
+#	if(eval("use LWP;")) {
+#		return($self->_LWPFetch($file));
+#	}
+	# Okay, LWP isn't available. Check for others
+	if(DP::GeneralHelpers::InPath('wget')) {
+		return($self->_WgetFetch($file));
+	}
+	if(DP::GeneralHelpers::InPath('curl')) {
+		return($self->_CurlFetch($file));
+	}
+	if(DP::GeneralHelpers::InPath('lynx')) {
+		return($self->_LynxFetch($file));
+	}
+	return('NOPROGRAM');
+}
+
+sub _LWPFetch {
+	my $self = shift;
+	my $file = shift;
+}
+
+# Purpose: Download a file from HTTP using lynx.
+# Usage: $self->_LynxFetch(FILE);
+sub _LynxFetch {
+	my $self = shift;
+	my $file = shift;
+	my $progress = shift;
+
+	# Have curl output the file to STDOUT and read from that.
+	# Ignore STDERR
+	my ($in, $out, $err);
+	my $ChildPID = open3($in,$out,$err,'lynx','-useragent=DP::GeneralHelpers::HTTPFetch//curl','-source',$file);
+	my $output;
+	while($output .= <$out> and not eof($out)) {
+		if($progress) {
+			$progress->('UNKNOWN');
+		}
+	}
+	close($in) if($in);
+	close($out) if($out);
+	close($err) if($err);
+	waitpid($ChildPID,WNOHANG);
+	my $ret = $? >> 8;
+	if($ret == 0) {
+		return($output);
+	} else {
+		return('FAIL');
+	}
+}
+
+# Purpose: Download a file from HTTP using curl.
+# Usage: $self->_CurlFetch(FILE);
+sub _CurlFetch {
+	my $self = shift;
+	my $file = shift;
+	my $progress = shift;
+
+	# Have curl output the file to STDOUT and read from that.
+	# Ignore STDERR
+	my ($in, $out, $err);
+	my $ChildPID = open3($in,$out,$err,'curl','--user-agent','DP::GeneralHelpers::HTTPFetch//curl','--fail','--location','--silent',$file);
+	my $output;
+	while($output .= <$out> and not eof($out)) {
+		if($progress) {
+			$progress->('UNKNOWN');
+		}
+	}
+	close($in) if($in);
+	close($out) if($out);
+	close($err) if($err);
+	waitpid($ChildPID,WNOHANG);
+	my $ret = $? >> 8;
+	if($ret == 0) {
+		return($output);
+	} else {
+		return('FAIL');
+	}
+}
+
+# Purpose: Download a file from HTTP using wget.
+# Usage: $self->_WgetFetch(FILE);
+sub _WgetFetch {
+	my $self = shift;
+	my $file = shift;
+	my $progress = shift;
+
+	# Have wget output the file to STDOUT and read from that. Ignore
+	# STDERR.
+	my ($in, $out, $err);
+	my $ChildPID = open3($in,$out,$err,'wget','--user-agent','DP::GeneralHelpers::HTTPFetch//curl','--quiet','-O','-',$file);
+	my $output;
+	while($output .= <$out> and not eof($out)) {
+		if($progress) {
+			$progress->('UNKNOWN');
+		}
+	}
+	close($in) if($in);
+	close($out) if($out);
+	close($err) if($err);
+	waitpid($ChildPID,WNOHANG);
+	my $ret = $? >> 8;
+	if($ret == 0) {
+		return($output);
+	} else {
+		return('FAIL');
+	}
+}
+
 1;
 __END__
 # Below is stub documentation for your module. You'd better edit it!
