f1c8977b95f417e651318833b2b0c890c771e951
Eskild Hustvedt <code@zerodogg.org>
2008-01-13 11:34:35 +0000

    Adding keywords and header

--- modules/DP-GeneralHelpers/lib/DP/GeneralHelpers/IPC.pm
+++ modules/DP-GeneralHelpers/lib/DP/GeneralHelpers/IPC.pm
@@ -1,3 +1,26 @@
+# DP::GeneralHelpers::IPC
+# $Id$
+# Copyright (C) Eskild Hustvedt 2007, 2008
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of either:
+# 
+#    a) the GNU General Public License as published by the Free
+#    Software Foundation; either version 3, or (at your option) any
+#    later version, or
+#    b) the "Artistic License" which comes with this Kit.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See either
+# the GNU General Public License or the Artistic License for more details.
+#
+# You should have received a copy of the Artistic License
+# in the file named "COPYING.artistic".  If not, I'll be glad to provide one.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program in a file named COPYING.gpl. 
+# If not, see <http://www.gnu.org/licenses/>.
 package DP::GeneralHelpers::IPC;
 use IO::Socket::UNIX;
 use Glib;
