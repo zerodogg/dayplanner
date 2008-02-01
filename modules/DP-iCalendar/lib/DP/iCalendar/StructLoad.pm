f648a31440c29fda1feb20b6e6cfc6d71290e34b
Eskild Hustvedt <code@zerodogg.org>
2008-02-01 18:50:06 +0000

    Properly object oriented, added _assertMustBeRef

--- modules/DP-iCalendar/lib/DP/iCalendar/StructLoad.pm
+++ modules/DP-iCalendar/lib/DP/iCalendar/StructLoad.pm
@@ -1,10 +1,11 @@
+#!/usr/bin/perl
 # DP::iCalendar::StructLoad
 # $Id$
 # An iCalendar structure loader
 # Copyright (C) Eskild Hustvedt 2008
 #
 # This program is free software; you can redistribute it and/or modify it
-# under the same terms as Perl itself. There is NO warranty;
+# under the same terms as Perl itthis. There is NO warranty;
 # not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 
 # This module is capable of loading any file conforming to some simple rules.
@@ -20,17 +21,29 @@
 use strict;
 use warnings;
 package DP::iCalendar::StructLoad;
+use constant { true => 1, false => 0 };
+
+our $VERSION;
+$VERSION = 0.1;
 
 sub new
 {
+	my $class = shift;
+	my $this = {};
+	bless($this,$class);
+	# The data structure will be saved here
+	$this->{data} = {};
+	$this->{ignoreAssertions} = false;
+	return $this;
 }
-sub load
+sub loadFile
 {
+	my $this = shift;
 	my $file = shift;
 	my $sLevel = 0;
 	my %Struct;
 	my @Nest;
-	my $CurrRef = \%Struct;
+	my $CurrRef = $this->{data};
 	my $PrevValue;
 	open(my $infile, '<',$file);
 	while($_ = <$infile>)
@@ -57,29 +70,16 @@ sub load
 		}
 		elsif ($key eq 'BEGIN')
 		{
-			# Check that it is a hash
-			if(not ref($CurrRef) eq 'HASH')
-			{
-				# It wasn't, work hard to get a useful error message
-				if(not ref($CurrRef))
-				{
-					if(defined($CurrRef))
-					{
-						die("FATAL: \$CurrRef turned out to NOT be a hashref, was an unknown var: $CurrRef");
-					}
-					else
-					{
-						die("FATAL: \$CurrRef turned out to NOT be a hashref, was UNDEF!");
-					}
-				}
-				else
-				{
-					die("FATAL: \$CurrRef turned out to NOT be a hash, was a: ".ref($CurrRef));
-				}
-			}
+			# Check that it is a hash - the assert function dies if it isn't
+			$this->_assertMustBeRef('HASH',$CurrRef,'CurrRef',true);
+
 			if (not defined $CurrRef->{$value})
 			{
 				$CurrRef->{$value} = [];
+			}
+			else
+			{
+				$this->_assertMustBeRef('ARRAY',$CurrRef,'CurrRef',true);
 			}
 			my $pushNo = push(@{$CurrRef->{$value}}, {});
 			$pushNo--;
@@ -107,11 +107,64 @@ sub load
 			_parseWarn("Line unparseable: $_");
 		}
 	}
-	#print Dumper(\%Struct);
 }
 sub _parseWarn
 {
 	warn($_[0]);
+}
+# Purpose: Assert if a variable is what we want it to be and display useful errors if it isn't
+# Usage: $this->_assertMustBeRef(refName,ref,varname,fatal);
+# 	refName = the reference type expected, ie. HASH or ARRAY
+# 	ref		= the variable that should be holding the reference
+# 	varname = the name of the variable that is passed as ref, this is used for printing useful messages
+# 	fatal	= true if we should die(), false if not
+# Returns true if it suceeds, false if not (and fatal=false)
+# NOTE that it can be turned completely off with $this->{ignoreAssertions} = true; - so do not
+# DEPEND upon its return value.
+sub _assertMustBeRef
+{
+	my $this = shift;
+	# Allow this to be disabled
+	if ($this->{ignoreAssertions})
+	{
+		return true;
+	}
+
+	my $refName = shift;
+	my $ref = shift;
+	if(not ref($ref) eq $refName)
+	{
+		my $varname = shift;
+		my $fatal = shift;
+
+		my $errMsg;
+		# It wasn't, work hard to get a useful error message
+		if(not ref($ref))
+		{
+			if(defined($ref))
+			{
+				$errMsg = "\$$varname turned out to NOT be a reference of type $refName, was an unknown var: $ref";
+			}
+			else
+			{
+				$errMsg = "\$$varname turned out to NOT be a reference of type $refName, was UNDEF!";
+			}
+		}
+		else
+		{
+			$errMsg = "\$$varname turned out to NOT be a reference of type $refName, was a: ".ref($ref);
+		}
+		if ($fatal)
+		{
+			$errMsg = "FATAL: ".$errMsg;
+			die($errMsg."\n");
+		}
+		else
+		{
+			warn("WARNING: ".$errMsg." - expect trouble\n");
+			return false;
+		}
+	}
 }
 #use Data::Dumper;
 #$Data::Dumper::Sortkeys=1;
