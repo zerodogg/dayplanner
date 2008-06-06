46f8a71e2eb0008e13ebb1211fb9d20d188f8d0f
Eskild Hustvedt <code@zerodogg.org>
2008-06-06 11:15:18 +0000

    Named release

--- modules/DP-CoreModules/lib/DP/CoreModules.pm
+++ modules/DP-CoreModules/lib/DP/CoreModules.pm
@@ -22,7 +22,7 @@ use warnings;
 use constant { true => 1, false => 0 };
 
 our $Version = '0.9.1';
-my $VersionName = 'SVN';
+my $VersionName = 'Senior';
 my %RuntimeModules;		# Keeps track of modules loaded during runtime
 
 # NOTE:
