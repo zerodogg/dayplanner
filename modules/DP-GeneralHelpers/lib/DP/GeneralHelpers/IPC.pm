fbae791e1c55df868deba97de6afbd01017d7bcd
Eskild Hustvedt <code@zerodogg.org>
2008-10-13 09:20:53 +0000

    License

--- modules/DP-GeneralHelpers/lib/DP/GeneralHelpers/IPC.pm
+++ modules/DP-GeneralHelpers/lib/DP/GeneralHelpers/IPC.pm
@@ -5,7 +5,7 @@
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of either:
 # 
-#    a) the GNU Lesser General Public License as published by the Free
+#    a) the GNU General Public License as published by the Free
 #    Software Foundation; either version 3, or (at your option) any
 #    later version, or
 #    b) the "Artistic License" which comes with this Kit.
@@ -13,13 +13,13 @@
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See either
-# the GNU Lesser General Public License or the Artistic License for more details.
+# the GNU General Public License or the Artistic License for more details.
 #
 # You should have received a copy of the Artistic License
 # in the file named "COPYING.artistic".  If not, I'll be glad to provide one.
 #
-# You should have received a copy of the GNU Lesser General Public License
-# along with this program in a file named COPYING.lgpl. 
+# You should have received a copy of the GNU General Public License
+# along with this program in a file named COPYING.gpl. 
 # If not, see <http://www.gnu.org/licenses/>.
 package DP::GeneralHelpers::IPC;
 use strict;
