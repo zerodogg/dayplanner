98cf13d6c340ceb7c5c9457917826b6306c06fad
Eskild Hustvedt <code@zerodogg.org>
2008-11-27 21:54:21 +0000

    Added initial documentation

--- modules/DP-CoreModules/lib/DP/CoreModules/Plugin.pm
+++ modules/DP-CoreModules/lib/DP/CoreModules/Plugin.pm
@@ -198,3 +198,209 @@ sub _get_currName
 	return $base;
 }
 1;
+__END__
+=head1 INTRODUCTION
+
+Day Planner offers a plugin system that lets you hook into most parts of the
+program. The basic syntax is inspired from Gtk2's signals.
+
+All plugins are perl objects. These objects communicate with Day Planner
+via a special plugin object that it gets supplied. This plugin object gives
+access to internal data, and handles signal passing.
+
+Day Planner comes with various plugins by default, ranging from the
+basic hello world to synchronization. Take a look at the source code
+of these plugins to get a feel for it.
+
+=head1 THE BASIC PLUGIN LAYOUT
+
+A plugin should have a new_instance method. This instance gets the
+plugin handler object supplied as its sole parameter.
+
+It should create the plugin's object and return it. It should only do
+signal connecting and the initialization that is required for it to be
+operational until the INIT signal is issued and it can finalize its
+initialization. See the builtin signals section for more information about
+the INIT signal.
+
+=head1 THE PLUGIN HANDLER OBJECT
+
+The plugin handler object is the main controller that handles all I/O
+between the main application and the plugin. It allows you to request
+data, set configuration parameters and hook into signals.
+
+It has the following methods:
+
+=over
+
+=item register_signals(B<ARRAY>)
+
+This method takes an array of zero or more elements as its parameter.
+It is used to register a signal (not a handler) for use. You should
+call this if your plugin will emit signals.
+
+=item signal_emit(B<STRING> signal)
+
+Emits the signal supplied, calling all listeners.
+
+=item signal_connect(B<STRING> signal, B<OBJECT> plugin, B<STRING> method)
+
+Connects the method on the plugin object supplied to the signal supplied.
+The method will then be called whenever the signal is emitted.
+
+=item set_var(B<STRING> variable name, B<VARIABLE> content)
+
+This is used for sharing data. Sets the variable supplied to the content
+supplied.
+
+=item get_var(B<STRING> variable name)
+
+Gets the contents of the shared variable supplied. Various signals
+can offer temporary access to various data structures. These are outlined
+in the documentation for the signal in question.
+
+=item delete_var(B<STRING> variable name)
+
+Deletes the variable supplied. You should never use this to delete
+content that is shared by anthing other than your plugin.
+
+=item set_tempvar(B<STRING> variable name, B<VARIABLE> content)
+
+This is the same as set_var() except that this version delete_var()s the
+variable after the next signal has been emitted, sharing the data only
+througout a single signal.
+
+=item set_confval(B<STRING> name, B<STRING> value)
+
+Sets the configuration value name to value for THIS plugin. The plugin
+object automatically handles making it unique for your plugin, so you do
+not need to ensure that the name is unique for the entire program, only
+for your plugin. The configuration values are automatically saved by
+Day Planner.
+
+=item get_confval(B<STRING> name)
+
+Gets the configuration value supplied.
+
+=item abort()
+
+Tells the plugin handler that the action that triggered the signal
+should stop processing after the signal has finished. Use with care.
+
+=back
+
+=head1 GLOBALLY SHARED VARIABLES
+
+=over
+
+=item MainWindow
+
+This is the main Gtk2::Window for Day Planner. Not available
+before the INIT signal.
+
+=item calendar
+
+The DP::iCalendar::Manager object.
+
+=item state
+
+A hashref to the state.conf configuration hash. You should not modify
+this, but use set/get_confval() instead.
+
+=item config
+
+A hashref to the dayplanner.conf configuration hash. This can not hold
+any configuration values other than those predefined. If you make changes
+make sure that they are valid.
+
+=item Gtk2Init
+
+Boolean, true if gtk2 has been initialized.
+
+=item i18n
+
+The DP::GeneralHelpers::i18n object.
+
+=item version
+
+The Day Planner version number.
+
+=item confdir
+
+The path to the Day Planner configuration directory.
+
+=back
+
+=head1 BUILTIN SIGNALS
+
+=over
+
+=item CREATE_MENUITEMS
+
+NOTE: This signal is emitted BEFORE the INIT signal.
+
+This lets you modify the Day Planner menus. It shares the following
+temporary variables:
+
+=over
+
+=item MenuItems
+
+An arrayref to an array containing elements for a Gtk2::ItemFactory.
+You can push additional items that you wish added to the menu onto this
+variable.
+
+=item HelpName
+
+The localized name of the help menu.
+
+=item EditName
+
+The localized name of the edit menu.
+
+=back
+
+=item INIT
+
+This is the initialization signal. If your module needs to perform some initial tasks,
+this is where it should be done.
+
+For instance, a synchronization plugin will want to do its initial synchronization
+in this signal.
+
+=item SAVEDATA
+
+Emitted right before Day Planner writes iCalendar data to disk. Cannot
+be aborted.
+
+=item SHUTDOWN
+
+Emitted right before Day Planner is about to exit (before the gtk2 main loop
+is exited, and before writing state and iCalendar files.
+
+=back
+
+=head1 SEE ALSO
+
+For program documentation: 
+L<dayplanner> L<dayplanner-daemon(1)> L<dayplanner-notifier(1)>
+
+For API documentation:
+L<DP::iCalendar> L<DP::iCalendar::Manager> L<Date::HolidayParser>
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright (C) Eskild Hustvedt 2006, 2007, 2008
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see L<http://www.gnu.org/licenses/>.
