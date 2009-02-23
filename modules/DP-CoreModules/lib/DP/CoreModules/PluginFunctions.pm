eb996509a9d77da5b78a273f456764aff078d1e3
Eskild Hustvedt <code@zerodogg.org>
2009-02-23 20:53:15 +0000

    Export DetectImage

--- modules/DP-CoreModules/lib/DP/CoreModules/PluginFunctions.pm
+++ modules/DP-CoreModules/lib/DP/CoreModules/PluginFunctions.pm
@@ -22,7 +22,7 @@ use Exporter qw(import);
 
 our @EXPORT_OK = qw(Assert);
 
-foreach my $sub (qw(DPIntWarn GTK_Flush DP_DestroyProgressWin DPError DPInfo DPCreateProgressWin runtime_use UpdatedData Gtk2_Button_SetImage DPQuestion))
+foreach my $sub (qw(DPIntWarn GTK_Flush DP_DestroyProgressWin DPError DPInfo DPCreateProgressWin runtime_use UpdatedData Gtk2_Button_SetImage DPQuestion DetectImage))
 {
 	eval('sub '.$sub.' { return main::'.$sub.'(@_); }');
 	push(@EXPORT_OK,$sub);
