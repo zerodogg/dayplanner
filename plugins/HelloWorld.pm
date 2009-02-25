84f4f76e52b4333f84840c48f0d8df57853ec8b9
Eskild Hustvedt <code@zerodogg.org>
2009-02-25 15:35:44 +0000

    Major improvements, show a bit more of what the plugin system can do

--- plugins/HelloWorld.pm
+++ plugins/HelloWorld.pm
@@ -14,7 +14,8 @@
 package DP::Plugin::HelloWorld;
 use strict;
 use warnings;
-use DP::CoreModules::PluginFunctions qw(DPInfo);
+use DP::CoreModules::PluginFunctions qw(DPInfo UpdatedData);
+use DP::iCalendar qw(iCal_ConvertFromUnixTime iCal_ParseDateTime iCal_GenDateTime);
 
 sub new_instance
 {
@@ -24,11 +25,15 @@ sub new_instance
 	my $plugin = shift;
 	$this->{plugin} = $plugin;
 	$this->{plugin}->signal_connect('INIT',$this,'helloWorld');
+	$this->{plugin}->signal_connect('POST_INIT',$this,'init_plugin_handlers');
+	# Clean up on exit
+	$this->{plugin}->signal_connect('SHUTDOWN',$this,'cleanWorld');
+
 	$this->{meta} =
 	{
 		name => 'HelloWorld',
 		title => '"Hello World" plugin example',
-		description => 'This is an example plugin, it is meant as a short example on how to write an extremely simple plugin for Day Planner. When active it will simply display a Hello World message each time Day Planner is started.',
+		description => 'This is an example plugin, it is meant as a short example on how to write an extremely simple plugin for Day Planner. When active it will display a Hello World message each time Day Planner is started, and add a temporary "hello world" event on the current date.',
 		version => 0.1,
 		apiversion => 1,
 		author => 'Eskild Hustvedt',
@@ -37,7 +42,44 @@ sub new_instance
 	return $this;
 }
 
+sub init_plugin_handlers
+{
+	my $this = shift;
+	# Clean up before a DPS sync
+	$this->{plugin}->signal_connect_ifavailable('DPS_PRE_SYNCHRONIZE',$this,'cleanWorld');
+	# Restart self after a sync
+	$this->{plugin}->signal_connect_ifavailable('DPS_POST_SYNCHRONIZE',$this,'helloWorldMkEvent');
+}
+
 sub helloWorld
 {
+	my $this = shift;
+	$this->helloWorldMkEvent();
 	DPInfo('Hello world');
+}
+
+sub helloWorldMkEvent
+{
+	my $this = shift;
+	my $ical = $this->{plugin}->get_var('calendar');
+	$this->cleanWorld;
+	my ($year,$month,$day,$time) = iCal_ParseDateTime(iCal_ConvertFromUnixTime(time));
+	my $dtstart = iCal_GenDateTime($year,$month,$day);
+	my %event = (
+		UID => 'DP-HelloWorldPluginString',
+		DTSTART => $dtstart,
+		SUMMARY => 'Hello world',
+	);
+	$ical->add(%event);
+	UpdatedData(1);
+}
+
+sub cleanWorld
+{
+	my $this = shift;
+	my $ical = $this->{plugin}->get_var('calendar');
+	if ($ical->exists('DP-HelloWorldPluginString'))
+	{
+		$ical->delete('DP-HelloWorldPluginString');
+	}
 }
